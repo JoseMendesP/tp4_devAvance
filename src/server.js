2faac13c82ab63194be43234196c0860f7c4b3a7
Jose Mendes <josemendesper24@gmail.com>
2024-02-14 12:20:16 +0100

    Ajout de etape 1 et 2

--- src/server.js
+++ src/server.js
@@ -1,56 +1,68 @@
-import Fastify from "fastify"
-import fastifyBasicAuth from "@fastify/basic-auth"
-
+import fastify from 'fastify';
+import fastifyBasicAuth from '@fastify/basic-auth';
+import fs from 'fs';
 
 const port = 3000;
-const authenticate = {realm: 'Westeros'}
+const authenticate = { realm: 'Westeros' };
 
-const fastify = Fastify({
-    logger: true
-})
+const app = fastify({
+    logger: true,
+    https: {
+        key: fs.readFileSync('server.key'),
+        cert: fs.readFileSync('server.crt')
+    }
+});
 
-fastify.register(fastifyBasicAuth, {
+app.register(fastifyBasicAuth, {
     validate,
     authenticate
-})
+});
 
 async function validate(username, password, req, reply) {
     if (username !== 'Tyrion' || password !== 'wine') {
-        return new Error('Winter is coming')
+        return new Error('Winter is coming');
     }
 }
 
-fastify.get('/dmz', {}, (req, res) => {
-    res.send({replique: "Ca pourrait être mieux protégé..."})
-})
+app.get('/dmz', {}, (req, reply) => {
+    reply.send({ replique: "Ca pourrait être mieux protégé..." });
+});
 
-fastify.after(() => {
-    fastify.route({
+app.after(() => {
+    app.route({
         method: 'GET',
         url: '/secu',
-        onRequest: fastify.basicAuth,
+        onRequest: app.basicAuth,
         handler: async (req, reply) => {
             return {
                 replique: 'Un Lannister paye toujours ses dettes !'
-            }
+            };
         }
-    })
-})
+    });
+    app.route({
+        method: 'GET',
+        url: '/autre',
+        handler: async (req, reply) => {
+            return {
+                message: 'Cette route est accessible sans authentification !'
+            };
+        }
+    });
+});
 
-fastify.setErrorHandler(function (err, req, reply) {
-
+app.setErrorHandler(function (err, req, reply) {
     if (err.statusCode === 401) {
-        console.log(err)
-        reply.code(401).send({replique: 'Tu ne sais rien, John Snow..'})
+        console.log(err);
+        reply.code(401).send({ replique: 'Tu ne sais rien, John Snow..' });
     }
-    reply.send(err)
-})
+    reply.send(err);
+});
 
-fastify.listen({port}, function (err, address) {
+app.listen(port, function (err, address) {
     if (err) {
-        fastify.log.error(err);
+        app.log.error(err);
         process.exit(1);
     }
 
-    fastify.log.info(`Fastify is listening on port: ${address}`);
+    app.log.info(`Fastify is listening on port: ${address}`);
 });
